dc3ae47a6d6a5d0844a970de30979d5ab59abf8f
gokaydmrl <gokaydemirel96@gmail.com>
2022-11-08 13:17:11 +0300

    rules

--- src/Rules/ruleList.ts
+++ src/Rules/ruleList.ts
@@ -1,7 +1,7 @@
 import { strInAr } from "../types";
 const ruleList: strInAr = [
   "to start the game please click start button",
-  "the sum of the numbers in all of the rows, columns and diagonals should be equal to the sum of the random numbers in red",
+  "the sum of the numbers in all of the rows, columns and diagonals should be equal to the sum of the random numbers that appear bold",
   "to see the result please click submit button",
   "you can reset game whenever you want by clicking reset button",
 ];
